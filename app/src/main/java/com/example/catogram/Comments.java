56626b67266fc0b8cd0ff8c502abfcc36103cd91
Bojana Kuzeva <kuzeva01@gmail.com>
2019-12-19 15:54:37 +0100

    changes for intent

--- app/src/main/java/com/example/catogram/Comments.java
+++ app/src/main/java/com/example/catogram/Comments.java
@@ -1,8 +1,11 @@
 package com.example.catogram;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
 
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
+import android.widget.ImageView;
 
 public class Comments extends AppCompatActivity {
 
@@ -10,5 +13,12 @@ public class Comments extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_comments);
+
+        ImageView commentImg = (ImageView)findViewById(R.id.mainImg);
+
+        Drawable drawable= ContextCompat.getDrawable(this,getIntent().getIntExtra(Post.IMAGE_KEY,0));
+
+        commentImg.setImageDrawable(drawable);
     }
+
 }
